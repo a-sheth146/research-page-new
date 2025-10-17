a1136fe6b53ffa8390f2bfb609af5b4f8683dd7a
ashni <ashni.sheth@turing.com>
2025-10-16 19:54:07 -0700

    fixed repo name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fixed-mockup/',
+  base: '/research-page-new/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
